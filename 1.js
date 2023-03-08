305e28ffe67ec26af5900a06fdb3ac17cda74f0d
Bella <bella.kamala@gmail.com>
2023-03-08 23:55:38 +0300

    added solved problems

---
+++ 1.js
@@ -0,0 +1,18 @@
+//Задача 1
+
+/* Техническое задание
+
+ Напиши программу, которая будет переводить градусы Фаренгейта в температуру по Цельсию.
+
+В переменной fahrenheitTemperature указаны градусы по Фаренгейту.
+
+Формула расчёта выглядит так: Tc = 5/9 * (Tf - 32).
+
+Результат вычислений запиши в переменную celsiusTemperature.
+
+*/
+
+
+let fahrenheitTemperature = 77;
+let celciusTemperature = 5 / 9 * (fahrenheitTemperature - 32);
+console.log(celciusTemperature);
